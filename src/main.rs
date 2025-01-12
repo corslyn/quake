a8d01b7ea2d9d755eda6b8d6542119764c461c66
Enzo Manzinali <enzomanzinali@protonmail.com>
2025-01-12 21:51:16 +0100

    Implement PAK header reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use pak::Pak;
+
+mod pak;
+
+fn main() -> Result<(), std::io::Error> {
+    let pak = Pak::new("id1/PAK0.PAK")?;
+    let header = Pak::read_header(&pak)?;
+    println!(
+        "Pak type : {}\nDirectory offset : {}\nDirectory size : {}",
+        header.id, header.dir_offset, header.dir_size
+    );
+    Ok(())
 }
---
+++ src/pak.rs
@@ -0,0 +1,51 @@
+use std::{fs::File, io, io::Read};
+
+use byteorder::{LittleEndian, ReadBytesExt};
+
+#[derive(Debug)]
+pub struct Pak {
+    /// Raw pak data
+    pub data: Vec<u8>,
+}
+
+pub struct PakHeader {
+    pub id: String,      // Should be "PACK"
+    pub dir_offset: u32, // Offset to the directory
+    pub dir_size: u32,   // Size of the directory
+}
+
+impl Pak {
+    /// Creates a PAK from a file
+    pub fn new(filepath: &str) -> Result<Self, io::Error> {
+        let mut pak_file = File::open(filepath)?;
+        let mut data: Vec<u8> = Vec::new();
+        pak_file.read_to_end(&mut data)?;
+        Ok(Pak { data })
+    }
+
+    pub fn read_header(&self) -> io::Result<PakHeader> {
+        let mut cursor = io::Cursor::new(&self.data);
+
+        // Read the first 4 bytes as a string
+        let mut id_buf = [0u8; 4];
+        cursor.read_exact(&mut id_buf)?;
+        let id = String::from_utf8_lossy(&id_buf).to_string();
+
+        if id != "PACK" {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Unsupported PAK file",
+            ));
+        }
+
+        // Read directory offset and size
+        let dir_offset = cursor.read_u32::<LittleEndian>()?;
+        let dir_size = cursor.read_u32::<LittleEndian>()?;
+
+        Ok(PakHeader {
+            id,
+            dir_offset,
+            dir_size,
+        })
+    }
+}
